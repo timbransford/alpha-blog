5af49290cf5a65e94a4238f3fe49d6ba52c47cfb
Tim Bransford remote <bransfordware@gmail.com>
2020-02-17 17:52:17 +0000

    Complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in for that action"
+      redirect_to root_path
+    end
+  end
+  
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 3)
@@ -53,7 +55,6 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-    @article = Article.find(params[:id])
       params.require(:article).permit(:title, :description, :user)
     end
 end
